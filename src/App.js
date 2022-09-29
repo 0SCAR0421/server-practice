5941e2d1f89caabcf80afef6aadb9cd2da22a548
0SCAR0421 <oscar0421@kakao.com>
2022-09-29 18:01:29 +0900

    로그인 테스팅

--- src/App.js
+++ src/App.js
@@ -8,13 +8,32 @@ function App() {
       password: 'test',
     },
     {
+      withCredentials: 'include',
+    })
+    console.log(res)
+  }
+  const tryLogin1 = async () => {
+    const res = await axios.post('https://8it.kro.kr:4000/api/v1/auth/signin',{
+      email: 'test@8it.kro.kr',
+      password: 'test',
+    },
+    {
       withCredentials: true,
+    })
+    console.log(res)
+  }
+  const tryLogin2 = async () => {
+    const res = await axios.post('https://8it.kro.kr:4000/api/v1/auth/signin',{
+      email: 'test@8it.kro.kr',
+      password: 'test',
     })
     console.log(res)
   }
   return (
     <div>
       <button onClick={tryLogin}>TEST</button>
+      <button onClick={tryLogin1}>TEST1</button>
+      <button onClick={tryLogin2}>TEST2</button>
     </div>
   );
 }
