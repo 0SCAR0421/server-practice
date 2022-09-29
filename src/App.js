db68bde813a8719b30d8b6f3cc10166e710dcb56
0SCAR0421 <oscar0421@kakao.com>
2022-09-29 17:42:49 +0900

    로그인 테스팅

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   const tryLogin = async () => {
-    const res = axios.post('https://8it.kro.kr:4000/api/v1/auth/signin',{
+    const res = await axios.post('https://8it.kro.kr:4000/api/v1/auth/signin',{
       email: 'test@8it.kro.kr',
       password: 'test',
     },
